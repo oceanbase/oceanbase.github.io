35a6c9537ab7eb55616cb6954d5d7725de08fd44
yuyi <yuyi.wsy@oceanbase.com>
2024-05-22 10:18:27 +0800

    feat(user): user logos with carousel

--- src/components/Users/index.tsx
+++ src/components/Users/index.tsx
@@ -1,10 +1,14 @@
-import Link from "@docusaurus/Link"
 import Translate from "@docusaurus/Translate"
 import useBaseUrl from '@docusaurus/useBaseUrl'
 
+import clsx from 'clsx'
 import citizen from './citizen.json'
 import coop from './coop.json'
 import overseas from "./overseas.json"
+
+import Carousel from 'react-bootstrap/Carousel'
+
+import './bs.css'
 
 type Partner = {
   name: string
@@ -17,27 +21,41 @@ const overseasPartners: Partner[] = overseas
 const citizenPartners: Partner[] = citizen
 
 function Logos(props: { partners: Partner[] }) {
+  const chunks: Partner[][] = props.partners.reduce((acc, item, index) => {
+    const chunkIndex = Math.floor(index / 9)
+    if (!acc[chunkIndex]) {
+      acc[chunkIndex] = []
+    }
+    acc[chunkIndex].push(item)
+    return acc
+  }, [])
+
   return (
-    <div className="container" style={{ paddingTop: 20 }}>
-      <div className="row">
-        {props.partners.map((partner, index) => (
-          <div key={index} className="col col--3">
-            <div className="text--center" style={{ marginTop: 32 }}>
-              <img className="partner__logo" src={useBaseUrl(partner.logoPath)} alt={partner.name} />
-            </div>
+    // <div className={clsx("container padding-vert--md shadow--lw margin-top--lg", styles.containerScroll)}>
+    <div className={clsx("container")}>
+      <Carousel nextLabel="" prevLabel="">
+        {chunks.map((chunk, index) => <Carousel.Item key={index}>
+          <div className="row" style={{ padding: 120, paddingTop: 80, paddingBottom: 80 }} >
+            {chunk.map((partner) => <div key={partner.name} className="col col--4">
+              <div className="text--center" style={{ marginBottom: 32, backgroundColor: 'white' }}>
+                <img className="partner__logo" src={useBaseUrl(partner.logoPath)} alt={partner.name} />
+              </div>
+            </div>)}
           </div>
-        ))}
-      </div>
+        </Carousel.Item>)}
+      </Carousel>
     </div>
   )
 }
 
 export function UsersComp(): JSX.Element {
   return <div style={{ marginBottom: 100 }}>
-    <div className="hero shadow--lw">
+    <div className="hero" style={{ paddingBottom: 0 }}>
       <div className="container" style={{ alignItems: "center", textAlign: "center" }}>
         <h1 className="hero__title"><Translate>Who is using</Translate> <span>OceanBase</span></h1>
-        <p>Please provide information on <Link href='https://github.com/oceanbase/oceanbase/issues/1301'>Who is using OceanBase</Link> to help improving OceanBase better.</p>
+        {/* <p>Please provide information on <Link href='https://github.com/oceanbase/oceanbase/issues/1301'>Who is using OceanBase</Link> to help improving OceanBase better.</p> */}
+        <p>Trusted by enterprises for mission-critical use cases</p>
+        <p>Payments systems, retailing, logistics, education and more - powered by OceanBase</p>
       </div>
     </div>
     {/* <Logos partners={coopPartners} /> */}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,29 +13,32 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext()
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
-      <div className="container">
-        <Heading as="h1" className="hero__title">
-          {siteConfig.title}
-        </Heading>
-        <p className="hero__subtitle">
-          <Translate>OceanBase is an enterprise distributed relational database developed by Ant Group.</Translate>
-        </p>
-        <div className={clsx(styles.buttons)}>
-          <Link
-            className="button button--secondary margin-vert--md"
-            to="docs/tutorial/intro"
-          >
-            <Translate>Learn about</Translate> OceanBase
-          </Link>
-          <span style={{display: 'inline-flex', alignItems: 'center'}}>
-            <a href="https://github.com/oceanbase/oceanbase" className="margin-horiz--sm" style={{display: 'inline-flex', alignItems: 'center'}}>
-              <img alt="stars" src="https://img.shields.io/badge/dynamic/json?color=white&label=stars&query=stargazers_count&url=https%3A%2F%2Fapi.github.com%2Frepos%2Foceanbase%2Foceanbase" />
-            </a>
-            <a href="https://github.com/oceanbase/oceanbase" className='margin-horiz--sm' style={{display: 'inline-flex', alignItems: 'center'}}>
-              <img alt="forks" src="https://img.shields.io/badge/dynamic/json?color=white&label=forks&query=forks_count&url=https%3A%2F%2Fapi.github.com%2Frepos%2Foceanbase%2Foceanbase" />
-            </a>
-          </span>
+      <div className={clsx("container", styles.flex)}>
+        <div>
+          <Heading as="h1" className={clsx("hero__title", styles.smallerTitle)}>
+            {siteConfig.title}
+          </Heading>
+          <p className={clsx("hero__subtitle", styles.smallerWhenMobile)}>
+            <Translate>OceanBase is an enterprise-grade open source distributed SQL database. Unlimited scalable distributed database for data-intensive transactional and real-time operational analytics workloads, with ultra-fast performance that has once achieved world records in the TPC-C benchmark test. OceanBase has served over 1000 customers across the globe and has been supporting all mission critical systems in Alipay.</Translate>
+          </p>
+          <div className={clsx(styles.buttons)}>
+            <Link
+              className="button button--secondary margin-vert--md"
+              to="docs/tutorial/intro"
+            >
+              <Translate>Learn about</Translate> OceanBase
+            </Link>
+            <span style={{display: 'inline-flex', alignItems: 'center'}}>
+              <a href="https://github.com/oceanbase/oceanbase" className="margin-horiz--sm" style={{display: 'inline-flex', alignItems: 'center'}}>
+                <img alt="stars" src="https://img.shields.io/badge/dynamic/json?color=white&label=stars&query=stargazers_count&url=https%3A%2F%2Fapi.github.com%2Frepos%2Foceanbase%2Foceanbase" />
+              </a>
+              <a href="https://github.com/oceanbase/oceanbase" className='margin-horiz--sm' style={{display: 'inline-flex', alignItems: 'center'}}>
+                <img alt="forks" src="https://img.shields.io/badge/dynamic/json?color=white&label=forks&query=forks_count&url=https%3A%2F%2Fapi.github.com%2Frepos%2Foceanbase%2Foceanbase" />
+              </a>
+            </span>
+          </div>
         </div>
+        <img src={require("./astro.png").default} alt="Astronaut Logo" className={styles.astro} />
       </div>
     </header>
   )
