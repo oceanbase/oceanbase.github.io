b7e79b70594faa81674ce0ad5f6edcb0afea8eed
yuyi <yuyi.wsy@oceanbase.com>
2024-05-23 18:02:29 +0800

    feat(blog): added links for blogs

--- src/components/Blogs/index.tsx
+++ src/components/Blogs/index.tsx
@@ -10,22 +10,22 @@ type BlogPost = {
 
 const posts: BlogPost[] = [
   {
-    title: "降低分布式数据库使用门槛，谈谈我们对小型化的思考",
-    image: "https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.",
-    link: "https://www.example.com",
+    title: "7 Key Technologies to Ensure High Availability in OceanBase Database",
+    image: "https://obportal.s3.ap-southeast-1.amazonaws.com/obc-blog/img/d105da79260f4d6a8a03571e4a2b17091682305027576.jpg",
+    description: "This article describes the high-availability technologies used in OceanBase Database and answers the some questions about OceanBase Database.",
+    link: "https://en.oceanbase.com/blog/2615184384",
   },
   {
-    title: "BOSS 直聘——日增10亿数据的历史库，如何通过OceanBase节省70%存储成本？",
+    title: "How Alipay Handles Traffic Surge during Double 11 with OceanBase",
     image: "https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.",
-    link: "https://www.example.com",
+    description: "Alipay was born in the “IOE” age when the conventional combination of IBM midrange computers, Oracle databases, and EMC storage devices was preferred by companies. The original database system of Alipay used a centralized architecture without sharding that unsurprisingly drowned in the traffic of the first Double 11 shopping festival.",
+    link: "https://en.oceanbase.com/blog/2596694784",
   },
   {
-    title: "OceanBase 4.3——实时分析AP的里程碑版本",
-    image: "https://images.unsplash.com/photo-1506624183912-c602f4a21ca7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60",
-    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.",
-    link: "https://www.example.com",
+    title: "Using OceanBase to build a real-time user analytics pipeline",
+    image: "https://obportal.s3.ap-southeast-1.amazonaws.com/obc-blog/img/434994947c5a4ee3a710cd277357c7c51694138540756.png",
+    description: "In the digital era we live in, data is everywhere — it’s collected from sources like web and mobile applications, IoT devices, social media interactions, and CRM systems. As a SaaS product owner myself, I’ve seen how this vast array of data sources can become overwhelming when it comes to user behavior analysis and visualization. The challenge is not just in collecting the data, but also in managing and making sense of it.",
+    link: "https://en.oceanbase.com/blog/5862202624",
   },
   {
     title: "Unveiling the Secrets of Microservices Architecture",
@@ -59,7 +59,7 @@ export const Blogs = (): JSX.Element => {
       <div className="container" style={{ marginBottom: 64 }}>
         <div className={"row"}>
           {posts.map((post, index) => <div className={"col col--4"} key={index}>
-            <div className={clsx("card", styles.card)} style={{ cursor: "pointer", marginBottom: 32 }}>
+            <a className={clsx("card", styles.card)} style={{ cursor: "pointer", marginBottom: 32 }} href={post.link} target="_blank">
               <div className="card__header">
                 {/* <h2>{index + 1}</h2> */}
                 <h3>{post.title}</h3>
@@ -69,14 +69,18 @@ export const Blogs = (): JSX.Element => {
                   src={post.image}
                   alt={post.title}
                   title={post.title}
+                  className={styles.cardImage}
                 />
               </div>
-              <div className="card__body">
-                <p>
+              <div className="card__body" style={{ height: 200, position: "relative" }}>
+                <p className={styles.truncateMultiline}>
                   {post.description}
                 </p>
+                <div className={styles.navArrow}>
+                  &gt;&gt;
+                </div>
               </div>
-            </div>
+            </a>
           </div>)}
         </div>
       </div>
