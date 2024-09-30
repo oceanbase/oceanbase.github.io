7d1bb6a135149fda9b19e7e625a46da7f50a5909
yuyi <yuyi.wsy@oceanbase.com>
2024-09-30 23:51:13 +0800

    fix(gitalk): fixed wrong confugration for gitalk

--- src/theme/DocPaginator/index.tsx
+++ src/theme/DocPaginator/index.tsx
@@ -1,10 +1,12 @@
-import 'gitalk/dist/gitalk.css'
+import 'gitalk/dist/gitalk.css';
 
+import BrowserOnly from '@docusaurus/BrowserOnly';
 import DocPaginator from '@theme-original/DocPaginator';
 import type DocPaginatorType from '@theme/DocPaginator';
-import GitalkComponent from "gitalk/dist/gitalk-component";
+import GitalkComponent from 'gitalk/dist/gitalk-component';
 import React from 'react';
-import type {WrapperProps} from '@docusaurus/types';
+import type { WrapperProps } from '@docusaurus/types';
+import {md5} from 'js-md5';
 
 type Props = WrapperProps<typeof DocPaginatorType>;
 
@@ -12,13 +14,20 @@ export default function DocPaginatorWrapper(props: Props): JSX.Element {
   return (
     <>
       <DocPaginator {...props} />
-      <GitalkComponent options={{
-        clientID: "Ov23li5A5dqaNIrrusQQ",
-        clientSecret: "c76f994e086901344abf11b597afbaa3e947e714",
-        repo: "docs-playground",
-        owner: "powerfooI",
-        admins: ["powerfooI"],
-      }} />
+      <BrowserOnly fallback={<div>Loading...</div>}>
+        {() => (
+          <GitalkComponent
+            options={{
+              clientID: 'Ov23li5A5dqaNIrrusQQ',
+              clientSecret: 'c76f994e086901344abf11b597afbaa3e947e714',
+              repo: 'docs-playground',
+              owner: 'powerfooI',
+              admin: ['powerfooI'],
+              id: md5(location.pathname),
+            }}
+          />
+        )}
+      </BrowserOnly>
     </>
   );
 }
